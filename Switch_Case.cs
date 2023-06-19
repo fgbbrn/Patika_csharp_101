796d9c92d1e368889c3c18f5a82a8554354dd960
Gültekin Furkan Baran <129402769+fgbbrn@users.noreply.github.com>
2023-06-19 16:42:04 +0300

    Switch-Case
    Patika csharp odev 6

---
+++ Switch_Case.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            //Expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındasınız");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Şubat ayındasınız");
+                    break ;
+                case 4:
+                    Console.WriteLine("Nisan ayındasınız");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart ayındasınız");
+                    break;
+                case 6:
+                    Console.WriteLine("Haziran ayındasınız");
+                    break;
+              
+                default:
+                    Console.WriteLine("Yanlış veri girişi");
+                    break;
+            }
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış mevsimindesiniz");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz mevsimindesiniz");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
